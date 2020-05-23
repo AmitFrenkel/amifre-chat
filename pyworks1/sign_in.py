f3bf7783bda9e608b5fd70508eaa8d9bcd2168ea
amit <amifre14@gmail.com>
2020-05-23 18:50:03 +0300

    update relative

--- pyworks1/sign_in.py
+++ pyworks1/sign_in.py
@@ -21,35 +21,35 @@ class Login:
     # The following function creates the login window objects.
     def create_window_objects(self):
         title = Label(self.__window, text='Login', bg='#025E73', fg='orange', font=('Segoe UI', 25))
-        title.place(x=150, y=20)
+        title.place(relx=0.415, rely=0.031)
         vcmd = (self.__window.register(validate), '%S')
         member_name = self.username(vcmd)
         password, show_password = self.user_password(vcmd)
-        submit_button = Button(self.__window, text='Submit', width=20, bg='green', bd=7, fg='#fbf168', relief=GROOVE,
+        submit_button = Button(self.__window, text='Submit', bg='green', bd=7, fg='#fbf168', relief=GROOVE,
                                font=('Segoe UI', 10), cursor='hand2', command=lambda: self.start_chat())
-        submit_button.place(x=120, y=300)
+        submit_button.place(relx=0.27, rely=0.46, relwidth=0.43)
         return member_name, password, show_password
 
     # The following function creates the password field.
     def user_password(self, vcmd):
         member_password = Label(self.__window, text='Password', bg='#025E73', fg='orange', font=('Segoe UI', 12))
-        member_password.place(x=20, y=140)
-        password = Entry(self.__window, width=25, bg='green', fg='orange', show='*',
+        member_password.place(relx=0.04, rely=0.21)
+        password = Entry(self.__window, bg='green', fg='orange', show='*',
                          font=('Segoe UI', 12), insertbackground='orange', validate='key', validatecommand=vcmd)
-        password.place(x=140, y=140)
-        show_password = Button(self.__window, text='Show Password', width=15, bg='green', bd=7, fg='#fbf168',
+        password.place(relx=0.27, rely=0.21, relwidth=0.5375)
+        show_password = Button(self.__window, text='Show Password', bg='green', bd=7, fg='#fbf168',
                                relief=GROOVE, font=('Segoe UI', 8), cursor='hand2',
                                command=lambda: self.show_password())
-        show_password.place(x=175, y=180)
+        show_password.place(relx=0.34, rely=0.27, relwidth=0.3225)
         return password, show_password
 
     # The following function creates the username field.
     def username(self, vcmd):
         l1 = Label(self.__window, text='Member Name', bg='#025E73', fg='orange', font=('Segoe UI', 12))
-        l1.place(x=20, y=100)
-        member_name = Entry(self.__window, width=25, bg='green', fg='orange',
+        l1.place(relx=0.04, rely=0.153)
+        member_name = Entry(self.__window, bg='green', fg='orange',
                             font=('Segoe UI', 12), insertbackground='orange', validate='key', validatecommand=vcmd)
-        member_name.place(x=140, y=100)
+        member_name.place(relx=0.27, rely=0.15, relwidth=0.5375)
         return member_name
 
     # The following function creates the tkinter window itself.
@@ -61,6 +61,8 @@ class Login:
         window.title('Sign In')
         window.iconbitmap('icon.ico')
         window.bind('<Return>', lambda x: self.start_chat())
+        print(window.winfo_screenheight())
+        print(window.winfo_screenwidth())
         window.resizable(height=False, width=False)
         return window
 
